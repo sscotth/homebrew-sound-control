2eec6f7bb245b732f53a9e5918f0b20afbb725b8
Scott Humphries <git@sscotth.io>
2017-10-11 14:55:42 -0500

    Update to v2.0.3

--- Casks/sound-control.rb
+++ Casks/sound-control.rb
@@ -1,6 +1,6 @@
 cask 'sound-control' do
-  version '2.0.2'
-  sha256 'fb847ea8288676745347673be088201bfc6293b9a9bca614f1e2bb01b5c6ba64'
+  version '2.0.3'
+  sha256 '342f42f5ebeb502ecf3b602670b5e51659c510d4c5cdb1a15e51afae4f1306a7'
 
   # staticz.net was verified as official when first introduced to the cask
   url "http://staticz.net/downloads/SoundControlInstaller_#{version}.dmg"
